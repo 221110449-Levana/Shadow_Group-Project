dd0318edef00a2ae8915a9996211f1d3b4ce90d8
Angelica Thomas <128479937+AngelicaThomas@users.noreply.github.com>
2025-11-08 20:20:34 +0700

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoForm from "./components/videoForm";
+
+function App() {
+  return (
+    <div>
+      <VideoForm />
+    </div>
+  );
+}
+
+export default App;
