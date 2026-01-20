32a12357be9625fec88e64009f8dec306fe02ed2
Angelica Thomas <128479937+AngelicaThomas@users.noreply.github.com>
2026-01-20 17:35:53 +0700

    Delete backend/src/server.js

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-// src/server.js
-require("dotenv").config();
-const app = require("./app");
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
